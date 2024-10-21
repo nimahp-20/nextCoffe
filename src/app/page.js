61844520d02f36702efcad5d4e4f57ff12f9354e
nima <nimaneon17@gmail.com>
2024-10-21 13:36:40 +0330

    Navbar and Slider + favIcon added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Navbar from "@/components/navbar/Navbar";
+import Banner from "@/templates/index/banner";
+
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Banner />
     </>
   );
 }
